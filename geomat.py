3a55307f9f9bfc3f7cb21b05a1c605c3b1995e2e
Alexander Swerdlow <aswerdlow1@gmail.com>
2022-03-09 12:38:04 -0800

    Remove utils

--- geomat.py
+++ geomat.py
@@ -10,52 +10,6 @@ from timm.data import resolve_data_config
 from PIL import Image
 import torchvision
 from torch_geometric.data import (Data, Dataset)
-
-_cache_ = {
-  'memory_allocated': 0,
-  'max_memory_allocated': 0,
-  'memory_reserved': 0,
-  'max_memory_reserved': 0,
-}
-
-def _get_memory_info(info_name, unit):
-
-  tab = '\t'
-  if info_name == 'memory_allocated':
-    current_value = torch.cuda.memory.memory_allocated()
-  elif info_name == 'max_memory_allocated':
-    current_value = torch.cuda.memory.max_memory_allocated()
-  elif info_name == 'memory_reserved':
-    tab = '\t\t'
-    current_value = torch.cuda.memory.memory_reserved()
-  elif info_name == 'max_memory_reserved':
-    current_value = torch.cuda.memory.max_memory_reserved()
-  else:
-    raise ValueError()
-
-  divisor = 1
-  if unit.lower() == 'kb':
-    divisor = 1024
-  elif unit.lower() == 'mb':
-    divisor = 1024*1024
-  elif unit.lower() == 'gb':
-    divisor = 1024*1024*1024
-  else:
-    raise ValueError()
-
-  diff_value = current_value - _cache_[info_name]
-  _cache_[info_name] = current_value
-
-  return f"{info_name}: \t {current_value} ({current_value/divisor:.3f} {unit.upper()})" \
-         f"\t diff_{info_name}: {diff_value} ({diff_value/divisor:.3f} {unit.upper()})"
-
-def print_memory_info(unit='kb'):
-
-  print(_get_memory_info('memory_allocated', unit))
-  print(_get_memory_info('max_memory_allocated', unit))
-  print(_get_memory_info('memory_reserved', unit))
-  print(_get_memory_info('max_memory_reserved', unit))
-  print('')
 class MyData(Data):
     def __cat_dim__(self, key, value, *args, **kwargs):
         if key == 'image':
